968e44e42e5e42cfd0d01242699de7bf4830a886
tatsuya-koyam26 <koyama.tatsuya@icloud.com>
2019-08-07 15:51:26 +0900

    add: routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/store'
-  get 'tasks/update'
-  get 'tasks/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :tasks do
+    collection do
+      post 'store'
+    end
+  end
 end
